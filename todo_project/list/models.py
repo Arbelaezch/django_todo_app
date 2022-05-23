893780eac1e2c91c977e4255d7cef6e4e45b8db7
Arbelaezch <arbelaezch@gmail.com>
2022-05-23 15:29:04 -0600

    Add List model

--- todo_project/list/models.py
+++ todo_project/list/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+class List(models.Model):
+	text = models.CharField(max_length=200)
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.text
+
+	def was_published_recently(self):
+		return self.created >= timezone.now() - datetime.timedelta(days=1)
