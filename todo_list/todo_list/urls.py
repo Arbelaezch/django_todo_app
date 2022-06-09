b073c6fb7f1aaa305d2ca1ff7980663d5428c278
Christian Arbelaez <christian.arbelaez@adminisorsmini3.su.ualberta.ca>
2022-06-09 15:59:17 -0600

    Adds base REST framework files

--- todo_list/todo_list/urls.py
+++ todo_list/todo_list/urls.py
@@ -1,22 +1,27 @@
-"""todo_list URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
 
+from rest_framework import routers, serializers, viewsets
+from todo_list.lists import views
+
+
+# Because we're using viewsets instead of views, we can automatically generate 
+# the URL conf for our API, by simply registering the viewsets with a router class. 
+# Again, if we need more control over the API URLs we can simply drop down to using 
+# regular class-based views, and writing the URL conf explicitly.
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include('lists.urls')),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
