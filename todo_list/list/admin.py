f3c2becb88388f0a84514d0ee8e422c57ced5e1a
Christian Arbelaez <christian.arbelaez@adminisorsmini3.su.ualberta.ca>
2022-06-09 15:01:23 -0600

    Creates .gitignore and fixes Item model

--- todo_list/list/admin.py
+++ todo_list/list/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Item
 
+class ListAdmin(admin.ModelAdmin): 
+    pass
+
+
+
+
+
 # Register your models here.
-admin.site.register(Item)
+admin.site.register(Item, ListAdmin)
--- todo_list/list/models.py
+++ todo_list/list/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=400)
     date = models.DateTimeField(auto_now_add=True)
-    is_complete = models.BinaryField(default=False)
+    is_complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
